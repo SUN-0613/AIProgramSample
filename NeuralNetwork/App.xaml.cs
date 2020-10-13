348ba0ba99f6420ff548c04d45ba9cc06be4d9a7
AYamamoto <a-yamamoto@rss-dev.co.jp>
2020-10-13 09:39:35 +0900

    プロジェクト作成

---
+++ NeuralNetwork/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace NeuralNetwork
+{
+
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+
+        /// <summary>処理開始</summary>
+        /// <param name="e">引数データイベント</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            base.OnStartup(e);
+
+
+
+        }
+
+    }
+
+}
