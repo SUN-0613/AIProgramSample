5d6ce3808ab8b88cbae53ef81293a7c7678f14a4
AYamamoto <a-yamamoto@rss-dev.co.jp>
2020-10-13 16:26:41 +0900

    MachinLearningプロジェクト追加

---
+++ MachineLearning/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace MachineLearning
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+
+        /// <summary>処理開始</summary>
+        /// <param name="e">引数データ</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            base.OnStartup(e);
+
+
+
+        }
+
+    }
+
+}
