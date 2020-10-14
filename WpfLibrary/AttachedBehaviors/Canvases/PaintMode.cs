a8e6d0124f7224d184aebbac13ffcee9a444107c
AYamamoto <a-yamamoto@rss-dev.co.jp>
2020-10-14 13:08:04 +0900

    WPFライブラリ用オブジェクト作成

---
+++ WpfLibrary/AttachedBehaviors/Canvases/PaintMode.cs
@@ -0,0 +1,214 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WpfLibrary.AttachedBehaviors.Canvases
+{
+
+    /// <summary>Canvasにフリーハンドで描画する機能を設ける添付ビヘイビア</summary>
+    /// <remarks>https://stackoverflow.com/questions/11010177/how-to-draw-a-free-hand-shape-random-text-printing-on-wpf-canvas</remarks>
+    public class PaintMode
+    {
+
+        #region dependency property
+
+        /// <summary>描画機能の有効化</summary>
+        public static readonly DependencyProperty IsEnabledProperty
+            = DependencyProperty.RegisterAttached(
+                "IsEnabled",
+                typeof(bool),
+                typeof(PaintMode),
+                new PropertyMetadata(false, OnIsEnabledChanged));
+
+        /// <summary>描画機能有効化の値を取得</summary>
+        /// <param name="sender">Canvas</param>
+        /// <returns>描画機能有効化の値</returns>
+        [AttachedPropertyBrowsableForType(typeof(Canvas))]
+        public static bool GetIsEnabled(DependencyObject sender)
+        {
+            return (bool)sender.GetValue(IsEnabledProperty);
+        }
+
+        /// <summary>描画機能有効化の値を設定</summary>
+        /// <param name="sender">Canvas</param>
+        /// <param name="value">設定値</param>
+        [AttachedPropertyBrowsableForType(typeof(Canvas))]
+        public static void SetIsEnabled(DependencyObject sender, bool value)
+        {
+            sender.SetValue(IsEnabledProperty, value);
+        }
+
+        /// <summary>描画する線の色</summary>
+        public static readonly DependencyProperty StrokeProperty
+            = DependencyProperty.RegisterAttached(
+                "Stroke",
+                typeof(Brush),
+                typeof(PaintMode),
+                new PropertyMetadata(Brushes.Black));
+
+        /// <summary>描画する線の色を取得</summary>
+        /// <param name="sender">Canvas</param>
+        /// <returns>描画する線の色</returns>
+        [AttachedPropertyBrowsableForType(typeof(Canvas))]
+        public static Brush GetStroke(DependencyObject sender)
+        {
+            return (Brush)sender.GetValue(StrokeProperty);
+        }
+
+        /// <summary>描画する線の色を設定</summary>
+        /// <param name="sender">Canvas</param>
+        /// <param name="value">描画する線の色</param>
+        [AttachedPropertyBrowsableForType(typeof(Canvas))]
+        public static void SetStroke(DependencyObject sender, Brush value)
+        {
+            sender.SetValue(StrokeProperty, value);
+        }
+
+        /// <summary>描画する線の太さ</summary>
+        public static readonly DependencyProperty StrokeThicknessProperty
+            = DependencyProperty.RegisterAttached(
+                "StrokeThickness",
+                typeof(double),
+                typeof(PaintMode),
+                new PropertyMetadata(2d));
+
+        /// <summary>描画する線の太さを取得</summary>
+        /// <param name="sender">Canvas</param>
+        /// <returns>描画する線の太さ</returns>
+        [AttachedPropertyBrowsableForType(typeof(Canvas))]
+        public static double GetStrokeThickness(DependencyObject sender)
+        {
+            return (double)sender.GetValue(StrokeThicknessProperty);
+        }
+
+        /// <summary>描画する線の太さを設定</summary>
+        /// <param name="sender">Canvas</param>
+        /// <param name="value">設定値</param>
+        [AttachedPropertyBrowsableForType(typeof(Canvas))]
+        public static void SetStrokeThickness(DependencyObject sender, double value)
+        {
+            sender.SetValue(StrokeThicknessProperty, value);
+        }
+
+        #endregion
+
+        #region global variable
+
+        /// <summary>マウスの現在位置</summary>
+        private static Point _MouseCurrentPoint;
+
+        /// <summary>線を描写するコントロール</summary>
+        private static Polyline _Line;
+
+        #endregion
+
+        #region event
+
+        /// <summary>描画機能有効化の値設定イベント</summary>
+        /// <param name="sender">Canvas</param>
+        /// <param name="e">プロパティ変更イベントデータ</param>
+        private static void OnIsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+            if (e.NewValue is bool value && value)
+            {
+
+                if (sender is Canvas canvas)
+                {
+
+                    canvas.Unloaded += OnUnloaded;
+                    canvas.MouseLeftButtonDown += OnMouseLeftButtonDown;
+                    canvas.MouseMove += OnMouseMove;
+
+                }
+
+            }
+            else
+            {
+                try
+                {
+                    OnUnloaded(sender, null);
+                }
+                catch { }
+            }
+
+        }
+
+        /// <summary>イベント解除</summary>
+        /// <param name="sender">Canvas</param>
+        /// <param name="e">イベントデータ</param>
+        private static void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+
+            if (sender is Canvas canvas)
+            {
+
+                canvas.Unloaded -= OnUnloaded;
+                canvas.MouseLeftButtonDown -= OnMouseLeftButtonDown;
+                canvas.MouseMove -= OnMouseMove;
+
+            }
+
+        }
+
+        /// <summary>マウス左ボタン押下</summary>
+        /// <param name="sender">Canvas</param>
+        /// <param name="e">マウスボタンイベントデータ</param>
+        /// <remarks>フリーハンドで描画開始</remarks>
+        private static void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            if (sender is Canvas canvas)
+            {
+
+                // 開始位置の記憶
+                _MouseCurrentPoint = e.GetPosition(canvas);
+
+                // 線コントロールのインスタンス生成
+                _Line = new Polyline()
+                {
+                    Stroke = GetStroke(canvas),
+                    StrokeThickness = GetStrokeThickness(canvas),
+                };
+
+                // 生成したインスタンスをCanvasに追加
+                canvas.Children.Add(_Line);
+
+            }
+
+        }
+
+        /// <summary>マウス移動イベント</summary>
+        /// <param name="sender">Canvas</param>
+        /// <param name="e">マウスイベントデータ</param>
+        /// <remarks>マウス左ボタン押下中はフリーハンドによる描画を継続</remarks>
+        private static void OnMouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (e.LeftButton.Equals(MouseButtonState.Pressed)
+                && sender is Canvas canvas)
+            {
+
+                // マウスの現在位置を取得
+                var current = e.GetPosition(canvas);
+
+                // 前回位置から現在位置までの線を描画
+                if (!_MouseCurrentPoint.Equals(current))
+                {
+
+                    _Line.Points.Add(current);
+                    _MouseCurrentPoint = current;
+
+                }
+
+            }
+
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ WpfLibrary/MVVM/ViewModelBase.cs
@@ -0,0 +1,138 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace WpfLibrary.MVVM
+{
+
+    /// <summary>ViewModelベースクラス</summary>
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+
+        #region event
+
+        /// <summary>プロパティ変更イベントハンドラ</summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region property
+
+        /// <summary>プロパティ更新実行済FLG</summary>
+        private bool _IsUpdatedProperties = false;
+
+        /// <summary>プロパティ更新実行済FLG</summary>
+        /// <remarks>
+        /// true :更新済
+        /// false:未更新
+        /// </remarks>
+        public bool IsUpdatedProperties
+        {
+            get => _IsUpdatedProperties;
+            private set
+            {
+                _IsUpdatedProperties = value;
+                CallPropertyChanged(false);
+            }
+        }
+
+        #endregion
+
+        #region global variable 
+
+        /// <summary>プロパティ更新実行済FLGの更新を行うか</summary>
+        private readonly bool _UpdateIsUpdatedProperties;
+
+        #endregion
+
+        #region instance
+
+        /// <summary>ViewModelベースクラス</summary>
+        public ViewModelBase()
+        {
+            _UpdateIsUpdatedProperties = false;
+        }
+
+        /// <summary>ViewModelベースクラス</summary>
+        /// <param name="updateIsUpdatedProperties">プロパティ更新実行済FLGの更新を行うか</param>
+        public ViewModelBase(bool updateIsUpdatedProperties)
+        {
+            _UpdateIsUpdatedProperties = updateIsUpdatedProperties;
+        }
+
+        #endregion
+
+        #region method
+
+        /// <summary>プロパティ更新実行済FLGのリセット</summary>
+        protected void ResetIsUpdatedProperties()
+        {
+
+            if (_UpdateIsUpdatedProperties)
+            {
+                _IsUpdatedProperties = false;
+            }
+
+        }
+
+        /// <summary>PropertyChanged()の実行</summary>
+        protected virtual void CallPropertyChanged()
+        {
+            CallPropertyChanged(2, true);
+        }
+
+        /// <summary>PropertyChanged()の実行</summary>
+        /// <param name="updateIsUpdatedProperties">プロパティ更新実行済FLGを更新するか</param>
+        protected virtual void CallPropertyChanged(bool updateIsUpdatedProperties)
+        {
+            CallPropertyChanged(2, updateIsUpdatedProperties);
+        }
+
+        /// <summary>PropertyChanged()の実行</summary>
+        /// <param name="stackFrameIndex">呼び出し元メソッドIndex</param>
+        protected virtual void CallPropertyChanged(int stackFrameIndex)
+        {
+            CallPropertyChanged(2, true);
+        }
+
+        /// <summary>PropertyChanged()の実行</summary>
+        /// <param name="stackFrameIndex">呼び出し元メソッドIndex</param>
+        /// <param name="updateIsUpdatedProperties">プロパティ更新実行済FLGを更新するか</param>
+        protected virtual void CallPropertyChanged(int stackFrameIndex, bool updateIsUpdatedProperties)
+        {
+
+            // 呼び出し元メソッド名をプロパティ名として取得
+            var caller = new StackFrame(stackFrameIndex);
+            var methodNames = caller.GetMethod().Name.Split('_');
+            var propertyName = methodNames[methodNames.Length - 1];
+
+            CallPropertyChanged(propertyName, updateIsUpdatedProperties);
+
+        }
+
+        /// <summary>PropertyChanged()の実行</summary>
+        /// <param name="propertyName">イベントを発生させたいプロパティ名</param>
+        protected virtual void CallPropertyChanged(string propertyName)
+        {
+            CallPropertyChanged(propertyName, true);
+        }
+
+        /// <summary>PropertyChanged()の実行</summary>
+        /// <param name="propertyName">イベントを発生させたいプロパティ名</param>
+        /// <param name="updateIsUpdatedProperties">プロパティ更新実行済FLGを更新するか</param>
+        protected virtual void CallPropertyChanged(string propertyName, bool updateIsUpdatedProperties)
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+            if (_UpdateIsUpdatedProperties && updateIsUpdatedProperties)
+            {
+                IsUpdatedProperties = true;
+            }
+
+        }
+
+        #endregion
+
+    }
+
+}
