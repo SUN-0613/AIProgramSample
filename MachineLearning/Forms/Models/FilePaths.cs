4efa3aed27f04d8487970c477099785f0dc67a52
AYamamoto <a-yamamoto@rss-dev.co.jp>
2020-10-16 16:49:14 +0900

    使用ファイルの管理クラスを作成

---
+++ MachineLearning/Forms/Models/FilePaths.cs
@@ -0,0 +1,22 @@
+﻿namespace MachineLearning.Forms.Models
+{
+
+    /// <summary>使用するファイルパス一覧</summary>
+    public class FilePaths
+    {
+
+        /// <summary>画像イメージファイルパス</summary>
+        public const string TrainImageFilePath = @"..\..\..\MNIST\train-images-idx3-ubyte\train-images.idx3-ubyte";
+
+        /// <summary>ラベルファイルパス</summary>
+        public const string TrainLabelFilePath = @"..\..\..\MNIST\train-labels-idx1-ubyte\train-labels.idx1-ubyte";
+
+        /// <summary>正解ラベルファイルパス</summary>
+        public const string LabelTextFilePath = @".\Label.txt";
+
+        /// <summary>ニューラルネットワークの構成と全体の重みを保存したファイル</summary>
+        public const string ParameterFilePath = @".\Weight.dat";
+
+    }
+
+}
